fe1372ca0a3b01fb0bdc5432d63d9d77b21f38fc
unknown <tassonilaura@gmail.com>
2019-05-20 16:05:14 -0400

    funciones clasicas

--- dia01/p2.py
+++ dia01/p2.py
@@ -1,7 +1,15 @@
 def sumatodos(limitto):
     resultado = 0
     for i in range(0,limitto+1):
-        resultado += 1
+        resultado += i
         
     return resultado
+
+def sumatodosloscuadrados(limitto):
+    resultado = 0
+    for i in range(limitto+1):
+        resultado += i*i
+    return resultado
+
 print(sumatodos(100))
+print(sumatodosloscuadrados(3))
